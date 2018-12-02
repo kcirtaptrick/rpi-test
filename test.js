2baf60fc4d09d265cee6e3068a04bd16d28e0a40
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-12-02 02:16:06 +0000

    added test images from webcam

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 var io = require('onoff').Gpio;
 var input = new io(4, 'both', 'in');
-
+console.log("Start");
 input.watch((err, val) => {
     if(err) {
         console.log(`Error: ${err}`);
         return;
     }
+    //console.log("Update");
     console.log(val);
 });
+var i = 0;  // dots counter
+setInterval(function() {
+  process.stdout.clearLine();  // clear current text
+  process.stdout.cursorTo(0);  // move cursor to beginning of line
+  i = (i + 1) % 10;
+  var dots = new Array(i + 1).join(".");
+  process.stdout.write("Waiting" + dots);  // write text
+}, 300);
