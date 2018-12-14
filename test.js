14ec05d815fc45953e8246545bfc188b16969451
Patrick Wang <patrick.bohan.wang@gmail.com>
2018-12-14 08:04:42 +0000

    troubleshooting

--- test.js
+++ test.js
@@ -16,28 +16,35 @@ function waiting(num = 3, speed = 300) {
         process.stdout.write("Waiting" + dots); // write text
     }, speed);
 }
-function testGPIO(min = 2, max = 26/*, options = {}*/) {
+
+function testGPIO(min = 2, max = 26 /*, options = {}*/ ) {
     var out = {
         pins: []
     }
     printObj(out)
-    for(let i = 0, pin = min; pin <= max; pin++) {
+    for (let i = 0, pin = min; pin <= max; pin++) {
         out.pins.push({
             number: pin,
             pin: new io(pin, 'out'),
             test: []
         });
         printObj(out);
-        
+
         out.pins[i].pin.writeSync(0);
-        out.pins[i].test.push({write: 0, read: out.pins[i].pin.readSync()});
+        out.pins[i].test.push({ write: 0, read: out.pins[i].pin.readSync() });
         out.pins[i].pin.writeSync(1);
-        out.pins[i].test.push({write: 1, read: out.pins[i].pin.readSync()});
+        out.pins[i].test.push({ write: 1, read: out.pins[i].pin.readSync() });
         i++;
     }
     return out;
 }
+
 function printObj(obj) {
     console.log(JSON.stringify(obj, null, 4))
-} 
-console.log(printObj(testGPIO()));
+}
+console.log(printObj(testGPIO().pins.map((x) => {
+            return {
+                pin: x.number,
+                test: x.test
+            }
+        })));
