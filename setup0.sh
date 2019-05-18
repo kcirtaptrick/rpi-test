d1aa3793bdb7330ead38bc6d4363e029c6707a47
kcirtaptrick <patrick.bohan.wang@gmail.com>
2019-05-18 13:40:02 -0700

    Check if node is already installed

--- setup0.sh
+++ setup0.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd /home/pi
-wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv6l.tar.xz
-tar -xzf node-v10.15.3-linux-armv6l.tar.xz
-cd node-v10.15.3-linux-armv6l
-sudo cp -R * /usr/local/
+if which node > /dev/null
+  then
+      echo "node is installed, skipping..."
+  else
+      cd /home/pi
+      wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv6l.tar.xz
+      tar -xzf node-v10.15.3-linux-armv6l.tar.xz
+      cd node-v10.15.3-linux-armv6l
+      sudo cp -R * /usr/local/
+      cd /home/pi
+      rm -rf node-v10.15.3-linux-armv6l*
+  fi
 sudo apt install pigpio
 cd $DIR
 npm install
