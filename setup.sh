3f66a03b4195960ea5cdf2104dbb63cb56ac96fd
kcirtaptrick <patrick.bohan.wang@gmail.com>
2019-05-18 13:25:56 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if which node > /dev/null
+  then
+      echo "node is installed, skipping..."
+  else
+      echo "Installing Node ..."
+      curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+      sudo apt-get install nodejs -y
+      echo "Node has been installed."
+  fi
+  
+sudo apt install pigpio
+cd $DIR
+npm install
